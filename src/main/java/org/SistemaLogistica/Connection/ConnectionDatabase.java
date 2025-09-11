2b084afa6d8831e91ade0500c4fd54ed796ec742
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-11 18:37:44 -0300

    feat: cpacotes e conexão com banco de dados

---
+++ src/main/java/org/SistemaLogistica/Connection/ConnectionDatabase.java
@@ -0,0 +1,16 @@
+package org.SistemaLogistica.Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDatabase {
+
+    private static final String URL = "jdbc:mysql://localhost:3356/SistemaLogistica?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection conectar() throws SQLException{
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}
--- src/main/java/org/SistemaLogistica/app/Main.java
+++ src/main/java/org/SistemaLogistica/app/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.SistemaLogistica.app;
 public class Main {
     public static void main(String[] args) {
 
