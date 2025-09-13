90b3a119082b3c9325b5d0a1a9cf2d18f7cbf010
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-12 21:50:21 -0300

    feat: Cria método TelaInicial

--- src/main/java/org/sistemalogistica/view/menus.java
+++ src/main/java/org/sistemalogistica/view/menus.java
@@ -1,4 +1,28 @@
 package org.sistemalogistica.view;
 
 public class menus {
+
+    public int TelaInicial(){
+
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃         Bem vindo ao RouteSystem         ┃");
+        System.out.println("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+        System.out.println("┃                 opções                   ┃");
+        System.out.println("┣------------------------------------------┫");
+        System.out.println("┃ 1 - Cadastros()                          ┃");
+        System.out.println("┃ 2 - Criar Pedido                         ┃");
+        System.out.println("┃ 3 - Gerar Entrega                        ┃");
+        System.out.println("┃ 4 - Histórico                            ┃");
+        System.out.println("┃ 5 - Atualizar Status Entrega             ┃");
+        System.out.println("┃ 6 - Listar Entregas                      ┃");
+        System.out.println("┃ 7 - Relatórios()                         ┃");
+        System.out.println("┃ 8 - Buscar Pedido                        ┃");
+        System.out.println("┃ 9 - Cancelar Pedido                      ┃");
+        System.out.println("┃ 10 - Excluir()                           ┃");
+        System.out.println("┃                                          ┃");
+        System.out.println("┃ 0 - sair                                 ┃");
+        System.out.println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+
+        return 0;
+    }
 }
