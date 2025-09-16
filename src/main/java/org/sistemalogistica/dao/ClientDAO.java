5e7f03ac63564cd2141556ab2f877c407794d231
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-15 21:39:10 -0300

    feat: Cria o dao de criação do cliente/motorista

--- src/main/java/org/sistemalogistica/dao/ClientDAO.java
+++ src/main/java/org/sistemalogistica/dao/ClientDAO.java
@@ -1,4 +1,28 @@
 package org.sistemalogistica.dao;
 
+import org.sistemalogistica.connection.ConnectionDatabase;
+import org.sistemalogistica.model.Client;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class ClientDAO {
+
+    public void createClient(Client client) throws Exception{
+
+        String query = "INSERT INTO Cliente (nome,cpf_cnpj,endereco,cidade,estado) VALUES (?,?,?,?,?)";
+
+        try (Connection conn = ConnectionDatabase.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setString(1, client.getName());
+            stmt.setString(2, client.getCpfCnpj());
+            stmt.setString(3, client.getAdress());
+            stmt.setString(4, client.getCity());
+            stmt.setString(5, client.getState());
+            stmt.executeUpdate();
+        }
+    }
+
+
 }
--- src/main/java/org/sistemalogistica/dao/DriverDAO.java
+++ src/main/java/org/sistemalogistica/dao/DriverDAO.java
@@ -1,4 +1,28 @@
 package org.sistemalogistica.dao;
 
+import org.sistemalogistica.connection.ConnectionDatabase;
+import org.sistemalogistica.model.Driver;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class DriverDAO {
+
+    public void createDriver(Driver driver) throws Exception{
+
+        String query = "INSERT INTO Motorista (nome,cnh,veiculo,cidade_base) VALUES (?,?,?,?)";
+
+        try (Connection conn = ConnectionDatabase.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setString(1, driver.getName());
+            stmt.setString(2, driver.getCnh());
+            stmt.setString(3, driver.getVehicle());
+            stmt.setString(4, driver.getBaseCity());
+            stmt.executeUpdate();
+
+        }
+    }
+
+
 }
