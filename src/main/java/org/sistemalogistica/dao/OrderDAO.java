266e52692a93bdbcbf39bc798d6c5ee0221415b4
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-15 21:53:30 -0300

    feat: inicia a criação do orderDAO

--- src/main/java/org/sistemalogistica/dao/OrderDAO.java
+++ src/main/java/org/sistemalogistica/dao/OrderDAO.java
@@ -1,4 +1,21 @@
 package org.sistemalogistica.dao;
 
+import org.sistemalogistica.connection.ConnectionDatabase;
+import org.sistemalogistica.model.Order;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class OrderDAO {
+
+    public void createOrder(Order order) throws Exception{
+
+        String query = "INSERT INTO Pedido (cliente_id,data_Pedido,volume_m3,peso_kg,status) VALUES (?,?,?,?,?)";
+
+        try(Connection conn = ConnectionDatabase.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setClientID();
+        }
+    }
 }
